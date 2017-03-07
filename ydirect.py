f20c8bc912c7505729581d93a58a82c8d61f2c85
I <ibelyalov@yandex.ru>
2017-03-08 01:41:21 +0300

    Refactor-refactor-refactor

--- ydirect.py
+++ ydirect.py
@@ -1,102 +1,120 @@
 # -*- coding: utf-8 -*-
-from grab import Grab
-from tqdm import tqdm
+import logging
 import re
-import time
+from collections import defaultdict
+from datetime import datetime
 
-# получаем количество страниц с объявлениями
-def get_pages(g, url, query):
-	print(query)
-	fullurl = url.format(query, 0)
-	g.go(fullurl)
+import unicodecsv as csv
+from tqdm import tqdm, trange
 
-	# проверка последней цифры
-	try:
-		pages = g.doc.select('//a[@class="b-pager__page"]')[-1].text()
-	except:
-		print('pass', fullurl)
+from grab import Grab
 
-	# если в конце стоит многоточие
-	if pages == '\u2026':
-		pages = g.doc.select('//a[@class="b-pager__page"]')[-2].text()
-		fullurl = url.format(query, int(pages))
-		g.go(fullurl)
-		pages = g.doc.select('//a[@class="b-pager__page"]')[-1].text()
-
-	return pages	
+logging.basicConfig(filename='{}.log'.format(datetime.now().strftime('%Y-%m-%d-%H:%M')), level=logging.ERROR)
 
 
-def main():
-	g = Grab()
-	g.setup(encoding='utf-8', connect_timeout=3, timeout=5)
 
-	# список запросов
-	words = [
-				'застройщики москвы и московской области',
-				'новостройки с отделкой в подмосковье от застройщика'
-			]
-	
-	url = 'http://direct.yandex.ru/search?&rid=213&text={0}&page={1}'
 
-	with open('firms.csv', 'wt', encoding="utf-8") as f:
-		f.write('"firm";"phone";"email";"title";"text";"domain"'+'\n')
-		
-		# номер телефона будем использовать за уникальное поле
-		uniq_phones = []
+g = Grab()
+g.setup(encoding='utf-8', connect_timeout=3, timeout=5)
+URL = 'http://direct.yandex.ru/search?&rid=213&text={0}&page={1}'
 
-		for query in words:
-			pages = get_pages(g, url, query)
 
-			for page in tqdm(range(int(pages))):
-				
-				fullurl = url.format(query, page)
-				g.go(fullurl)
-				
-				for item in g.doc.select('//div[@class="banner-selection"]'):
+def get_pages(query):
+    """
+    Количество страниц с объявлениями
+    """
 
-					# получаем заголовок и текст объявления
-					ad_title = re.sub(r'"', '', item.select('./div[@class="ad"]/div[@class="ad-link"]').text())
-					ad_body = re.sub(r'"', '', item.select('./div[@class="ad"]/div')[-1].text())
+    print(query)
+    full_url = URL.format(query, 0)
+    g.go(full_url)
 
-					# пробуем получить название компании, телефон и почту
-					try:
-						# получаем ссылку на карточку объявления
-						ad_url = item.select('./div[@class="ad"]/span/a[@class="vcard"]').attr('href')
-						g1 = Grab()
-						g1.go(ad_url)
+    # проверка последней цифры
+    sel = g.doc.select('//a[@class="b-pager__page"]')
+    if sel.exists():
+        pages = sel[-1].text()
+    else:
+        print('pass', full_url)
 
-						try:
-							ad_h1 = re.sub(r'"', '', g1.doc.select('//h1').text())
-						except:
-							ad_h1 = ''
+    # если в конце стоит многоточие
+    if pages == '\u2026':
+        pages = g.doc.select('//a[@class="b-pager__page"]')[-2].text()
+        full_url = URL.format(query, int(pages))
+        g.go(full_url)
+        pages = g.doc.select('//a[@class="b-pager__page"]')[-1].text()
 
-						try:
-							ad_phone = g1.doc.select('//div[@class="contact-item call-button-container"]/div[@class="large-text"]').text()
-						except:
-							ad_phone = ''
+    return pages
 
-						try:
-							ad_email = g1.doc.select('//a[@class="email"]').text()
-						except:
-							ad_email = ''
-					except:
-						ad_h1 = ''
-						ad_phone = ''
-						ad_email = ''
 
-					# пробуем получить адрес сайта
-					try:
-						ad_domain = item.select('./div[@class="ad"]/span/span[@class="domain"]').text()
-					except:
-						ad_domain = ''
+def main(queries, filename='firms.csv'):
+    """
+    Качаем все объявления в файл
+    Args:
+        queries: Список фраз для скрепинга.
+        filename: Имя файла, в который сохраняем.
+    """
+    with open(filename, 'wb') as f:
+        fieldnames = ['firm', 'phone', 'email', 'title', 'text', 'domain']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
 
-					# если объявление с таким номером уже есть, не добавляем его
-					if ad_phone in uniq_phones:
-						pass
-					else:
-						f.write('"'+ad_h1+'";"'+ad_phone+'";"'+ad_email+'";"'+ad_title+'";"'+ad_body+'";"'+ad_domain+'"'+'\n')
-						uniq_phones.append(ad_phone)
-		f.close()
+        # номер телефона будем использовать за уникальное поле
+        uniq_phones = []
+
+        for query in queries:
+            pages = get_pages(query)
+
+            for page in trange(int(pages), desc='pages'):
+                full_url = URL.format(query, page)
+                g.go(full_url)
+
+                for item in tqdm(g.doc.select('//div[@class="banner-selection"]'), desc='ads'):
+                    ad = defaultdict(str)
+
+                    # получаем заголовок, текст объявления
+                    xpath_elem_map = {
+                        './div[@class="ad"]/div[@class="ad-link"]': 'title',
+                        './div[@class="ad"]/div': 'text',
+                        './div[@class="ad"]/span/span[@class="domain"]': 'domain',
+                        './div[@class="ad"]/span/a[@class="vcard"]/@href': 'url'
+                    }
+
+                    for xpath, elem in xpath_elem_map.items():
+                        sel = item.select(xpath)
+                        if sel.exists():
+                            ad[elem] = sel.text()
+
+                    try:
+                        # получаем ссылку на карточку объявления
+                        g1 = Grab()
+                        g1.go(ad['url'])
+
+                        xpath_elem_map = {
+                            '//h1': 'firm',
+                            '//div[@class="contact-item call-button-container"]/div[@class="large-text"]': 'phone',
+                            '//a[@class="email"]': 'email'
+                        }
+
+                        for xpath, elem in xpath_elem_map.items():
+                            sel = g1.doc.select(xpath)
+                            if sel.exists():
+                                ad[elem] = sel.text()
+
+                    except Exception as e:
+                        logging.error(e)
+
+                    # не хотим сохранять url
+                    del ad['url']
+
+                    # если объявление с таким номером уже есть, не добавляем его
+                    if ad['phone'] not in uniq_phones:
+                        writer.writerow(ad)
+                        uniq_phones.append(ad['phone'])
 
 if __name__ == "__main__":
-    main()
+    # список запросов
+    words = [
+        'застройщики москвы и московской области',
+        'новостройки с отделкой в подмосковье от застройщика'
+    ]
+
+    main(words)
